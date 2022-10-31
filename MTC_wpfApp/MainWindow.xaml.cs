5ebee74c363b1aa3fde0f0e373d0ba701be6246d
Rustem <ryuzikc@gmail.com>
2022-11-01 00:43:15 +0300

    Добавьте файлы проекта.

---
+++ MTC_wpfApp/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MTC_wpfApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            clientGrid.ItemsSource = Models.MTCEntities.GetContext().Clients.ToList();
+        }
+
+        private void openClientwindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            Windows.CreateClient window = new Windows.CreateClient();
+            window.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ MTC_wpfApp/Windows/CreateClient.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MTC_wpfApp.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для CreateClient.xaml
+    /// </summary>
+    public partial class CreateClient : Window
+    {
+        public CreateClient()
+        {
+            InitializeComponent();
+        }
+
+        private void createClientButton_Click(object sender, RoutedEventArgs e)
+        {
+            Models.Client client = new Models.Client();
+            client.name = NameTextBox.Text;
+            client.surname = SurnameTextBox.Text;
+            client.patronymic = PatronymicTextBox.Text;
+            client.phone = PhoneTextBox.Text;
+            client.adress = AdressTextBox.Text;
+            client.reg_date = DateTime.Now;
+            Models.MTCEntities.GetContext().Clients.Add(client);
+            Models.MTCEntities.GetContext().SaveChanges();
+
+            MainWindow window = new MainWindow();
+            window.Show();
+            this.Hide();
+        }
+    }
+}
